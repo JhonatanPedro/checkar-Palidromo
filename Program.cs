bfd18e517ba7d4571e192aaaee67ea99904b0c37
JhonatanPedro <jhonatanpedro016@gmail.com>
2023-09-13 19:57:16 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class PolidromeChecker
+{
+    static bool isPolidrome(string text)
+    {
+        text = text.Replace(" ","").ToLower();
+        char[] caracters = text.ToArray();
+        Array.Reverse(caracters);
+        string invertd = new string(caracters);
+
+        return text == invertd;
+    }
+
+
+    static void Main(){
+        Console.Write("Digite uma palavra: ");
+        string text = Console.ReadLine();
+
+        if(isPolidrome(text))
+        {
+            Console.WriteLine("Sua palavra é um palidromo.");
+        }
+        else
+        {
+            Console.WriteLine("Sua palavra não é um palidromo.");
+        }
+    }
+
+
+}
